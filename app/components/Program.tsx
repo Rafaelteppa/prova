27eb481584d384a5f6b004ff10bcb2cad2d22d38
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-05-03 02:18:55 +0000

    fix: progran,leyout

--- app/components/Program.tsx
+++ app/components/Program.tsx
@@ -1,24 +1,71 @@
 export default function Program(){
     return(
-        <><div className="mt-5 grid">
+        <><div className="mt-5 grid text-center">
             <h2>EXPLORE OUR PROGRAM</h2>
         </div>
         
-        <div className=" mt-6 bg-black">
-                <div className="  md:flex justify-text lg:mt-8  text-white ">
-                    <p>
-
-
-                        Embrace the essence of strength as we delve into its various dimensions physical, mental, and emotional.
+        <div className="mt-6  gap-6 grid grid-cols-1 md:grid-cols-4">
+                 <div className=" bg-gray-700 gap-4 grid">
+                    <div className="mt-4 mx-4">
+                    <img src="/img/luva.svg" alt="" />
+                    <h1 className="mt-4 font-bold">Strength</h1>
+                    <p className="mt-4">
+                        Embrace the essence of strength as we
+                        delve into its various dimensions
+                        physical, mental, and emotional.
                     </p>
-                    <div className=" lg:mt-8">
-                        <img src="/img/coraçao.svg" alt="" />
-
-
-
+                    <div className="mt-4">
+                    <a href="#">Join Now</a>
+                    </div>
                     </div>
                 </div>
-            </div></>
+
+                <div className=" bg-gray-700 gap-4 grid">
+                    <div className="mt-4 mx-4">
+                    <img src="/img/coraçao.svg" alt="" />
+                    <h1 className="mt-4 font-bold">Strength</h1>
+                    <p className="mt-4">
+                        Embrace the essence of strength as we
+                        delve into its various dimensions
+                        physical, mental, and emotional.
+                    </p>
+                    <div className="mt-4">
+                    <a href="#">Join Now</a>
+                    </div>
+                    </div>
+                </div>
+
+                <div className=" bg-gray-700 gap-4 grid">
+                    <div className="mt-4 mx-4">
+                    <img src="/img/caminhada.svg" alt="" />
+                    <h1 className="mt-4 font-bold">Strength</h1>
+                    <p className="mt-4">
+                        Embrace the essence of strength as we
+                        delve into its various dimensions
+                        physical, mental, and emotional.
+                    </p>
+                    <div className="mt-4">
+                    <a href="#">Join Now</a>
+                    </div>
+                    </div>
+                </div>
+
+                <div className=" bg-gray-700 gap-4 grid">
+                    <div className="mt-4 mx-4">
+                    <img src="/img/cesta.svg" alt="" />
+                    <h1 className="mt-4 font-bold">Strength</h1>
+                    <p className="mt-4">
+                        Embrace the essence of strength as we
+                        delve into its various dimensions
+                        physical, mental, and emotional.
+                    </p>
+                    <div className="mt-4">
+                    <a href="#">Join Now</a>
+                    </div>
+                    </div>
+                </div>
+        </div>
+            </>
        
 
        
