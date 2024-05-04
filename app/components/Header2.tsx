2ca459c2a00a04071998c7a2547a88b8526416b0
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-05-04 01:26:59 +0000

    fix: Header2

--- app/components/Header2.tsx
+++ app/components/Header2.tsx
@@ -1,23 +1,58 @@
 export default function Header() {
     return (
-        <div className="text-center mt-4 mx-4 ">
+        <div className="mt-10 text-center">
             <div>
-             <h1 className="font-bold"> WHY JOIN US ?</h1>
-
+                <h1 className="font-bold"> WHY JOIN US ?</h1>
             </div>
             <div className="mt-4">
-             <h2>Our diverse membership base creates a
-             friendly and supportive atmosphere, where
-             you can make friends and stay motivated  </h2>
-
+                <h2>
+                    Our diverse membership base creates a
+                    friendly and supportive atmosphere, where
+                    you can make friends and stay motivated  
+                </h2>
             </div>
-            <div className="mt-7 grid justify-items-center bg-auto flex-auto">
+
+            <div>
                 <img src="/img/mulher.png" alt="" />
+                <div className="text-left bg-gray-700 rounded-lg md:grid grid-cols-3">
+                    <div className="flex items-center gap-4">
+                        <div>
+                            <img src="/img/grid1.png" alt="" />
+                        </div>
+                        <div className="ml-4">
+                            <h1 className="font-bold mt-4">Personal Trainer</h1>
+                            <h4 className="mt-4">
+                                Unlock your potential with
+                                our expert Personal Trainers.
+                            </h4>   
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>
+                            <img src="/img/grid2.png" alt="" />
+                        </div>
+                        <div className="ml-4">
+                            <h1 className="font-bold mt-4">Practice Sessions</h1>
+                            <h4 className="mt-4">
+                                Elevate your fitness with
+                                practice sessions.
+                            </h4>   
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <div>
+                            <img src="/img/grid3.png" alt="" />
+                        </div>
+                        <div className="ml-4">
+                            <h1 className="font-bold mt-4">Good Management</h1>
+                            <h4 className="mt-4">
+                                Supportive management,
+                                for your fitness success.
+                            </h4>   
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
-
     )
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
-
-
 import Program from "./components/Program";
+
+
 import Header2 from "./components/Header2";
 //import Personal from "./components/Personal";
-import Join_Grid from "./components/Join_Grid";
+// import Join_Grid from "./components/Join_Grid";
 import Footer_Bar from "./components/Footer_Bar";
 import Footer from "./components/Footer";
 
@@ -16,7 +16,7 @@ export default function Home() {
       <Banner/>
       <Program/>
       <Header2/>
-     <Join_Grid/>
+     {/* <Join_Grid/> */}
      <Footer/>
      <Footer_Bar/>
     </div>
