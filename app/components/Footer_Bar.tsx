e233231184570a9328568f2676a3a634413a529b
Rafaelteppa <167258743+Rafaelteppa@users.noreply.github.com>
2024-05-04 01:33:09 +0000

    fix:Footer_Bar

--- app/components/Footer_Bar.tsx
+++ app/components/Footer_Bar.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer_Bar(){
     return(
-        <div className="mt-12 text-center">
+        <div className="mt-12 text-center md:flex">
             <h6>Copyright @ 2023 UTFPR. All rights reserved.</h6>
         </div>
     )
